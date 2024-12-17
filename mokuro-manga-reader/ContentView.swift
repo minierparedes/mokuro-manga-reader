f636f6ccbdd75ca9af715c4ff7b988857bff5625
minierparedes <3thancr0wn@gmail.com>
2024-12-17 14:51:17 +0900

    Initial Commit

---
+++ mokuro-manga-reader/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  mokuro-manga-reader
+//
+//  Created by ethan crown on 2024/12/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ mokuro-manga-reader/mokuro_manga_readerApp.swift
@@ -0,0 +1,17 @@
+//
+//  mokuro_manga_readerApp.swift
+//  mokuro-manga-reader
+//
+//  Created by ethan crown on 2024/12/17.
+//
+
+import SwiftUI
+
+@main
+struct mokuro_manga_readerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
